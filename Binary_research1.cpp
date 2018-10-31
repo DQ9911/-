3b7fce4388ffff3b292c533281675cfc68ec80fe
DQ9911 <43775609+DQ9911@users.noreply.github.com>
2018-10-31 10:25:10 +0800

    Create Binary_research1.cpp

---
+++ Binary_research1.cpp
@@ -0,0 +1,12 @@
+double f(double x) {
+    return exp(x) + x - 5;
+}
+void solve() {
+    double l = -1000, r = 1000, eps = 1e-6;
+    while(r - l < eps) {
+        double mid = (l + r) / 2;
+        if(f(mid) > 0) r = mid;
+        else l = mid;
+    }
+    return l;
+}
