3680605d6a9137ff0b5867fc01a17d3d784052a4
DQ9911 <43775609+DQ9911@users.noreply.github.com>
2018-10-31 10:18:23 +0800

    Update quick_pow.cpp

--- quick_pow.cpp
+++ quick_pow.cpp
@@ -1,6 +1,12 @@
+const int mod = 1e9 + 7;
+typedef long long ll;
 ll quick_pow(ll a, ll b) {
-    ll ret = 1; assert(b >= 0);
-    for(a %= mod; b; b >>= 1, a = a * a % mod)
+    ll ret = 1; 
+    a %= mod;
+    while(b) {
         if(b & 1) ret = ret * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
     return ret;
 }
