8bde549a4039cff955b1cacf52ca839e901a324a
DQ9911 <43775609+DQ9911@users.noreply.github.com>
2018-11-04 14:59:11 +0800

    Update quick_pow.cpp

--- quick_pow.cpp
+++ quick_pow.cpp
@@ -1,8 +1,8 @@
 const int mod = 1e9 + 7;
-typedef long long ll;
+typedef long long ll; //使用long long类型很重要...
 ll quick_pow(ll a, ll b) {
     ll ret = 1; 
-    a %= mod;
+    a %= mod;//防止下面a*a溢出
     while(b) {
         if(b & 1) ret = ret * a % mod;
         a = a * a % mod;
