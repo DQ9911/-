bf8547f7b37c90b7c6faa13558a24cd4c7566487
DQ9911 <43775609+DQ9911@users.noreply.github.com>
2018-11-04 15:25:00 +0800

    Update Binary_research+greedy.cpp

--- Binary_research+greedy.cpp
+++ Binary_research+greedy.cpp
@@ -1,12 +1,13 @@
+//整数分组...
 const int N = 1e5 + 7;
 int n, k;
 int a[N];
-bool chk(int x) {
+bool chk(int x) { //判断x是否合法
     int t = 0, cnt = 0;
     for(int i = 0; i < n; i++)
-        if((t += a[i]) >= x)
+        if((t += a[i]) >= x)//贪心分组..
             t = 0, cnt++;
-    return cnt >= k;
+    return cnt >= k; //如果组数大于等于k返回true
 }
 void solve() {
     int l = 0, r = 1001, mid, ans = 0;
