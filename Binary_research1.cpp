b5c76ec1abde0a1b081107d4b5a9999ee8207ead
DQ9911 <43775609+DQ9911@users.noreply.github.com>
2018-11-04 15:19:35 +0800

    Update Binary_research1.cpp

--- Binary_research1.cpp
+++ Binary_research1.cpp
@@ -2,7 +2,7 @@ double f(double x) {
     return exp(x) + x - 5;
 }
 void solve() {
-    double l = -1000, r = 1000, eps = 1e-6;
+    double l = -1000, r = 1000, eps = 1e-6;//eps表示精度...
     while(r - l < eps) {
         double mid = (l + r) / 2;
         if(f(mid) > 0) r = mid;
