90a4db8f4218473f67bf90528c6ba65b2a3a8fee
DQ9911 <43775609+DQ9911@users.noreply.github.com>
2018-11-04 15:18:29 +0800

    Update euler_sieve.cpp

--- euler_sieve.cpp
+++ euler_sieve.cpp
@@ -4,9 +4,9 @@ int p[N / 8], pn = 0;
 void sieve() {
     for(int i = 2; i < N; i++) {
         if(!np[i]) p[pn++] = i;
-        for(int j = 0; j < pn && i * p[j] < N; j++) {
+        for(int j = 0; j < pn && i * p[j] < N; j++) { //将最小素因数为p[j] 的 i * p[j]标记为非质数....
             np[i * p[j]] = true;
-            if(i % p[j] == 0) break;
+            if(i % p[j] == 0) break; //下一个质数不可能是i * p[j]的最小素因数...
         }
     }
 }
